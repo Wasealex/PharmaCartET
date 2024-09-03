1c9ed5e8daecc8b2f5d08d4cea4d7b06594a4535
Wasealex <wasealex27@gmail.com>
2024-09-03 10:38:12 +0300

    feat: Add user authentication route and controller

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 const port = process.env.PORT || 5000;
-
+import userRoutes from "./routes/userRoutes.js";
 const app = express();
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is ready"));
 app.listen(port, () => console.log(`Server Started on port ${port}`));
