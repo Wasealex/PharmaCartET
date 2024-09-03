ee11039cf71c4e606c9b3e38593127e42d2eaeeb
Wasealex <wasealex27@gmail.com>
2024-09-03 10:38:54 +0300

    chore: Update userController.js to use expressAsyncHandler for error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ import expressAsyncHandler from "express-async-handler";
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
-const authUser = asyncHandler(async (req, res) => {
+const authUser = expressAsyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 });
 
