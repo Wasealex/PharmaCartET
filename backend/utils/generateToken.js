addd51dca027dd12fb8f4042c6a3898791b537ac
Wasealex <wasealex27@gmail.com>
2024-09-17 17:32:14 +0300

    refactor: Update generateToken.js to set secure flag to true for cookie in production

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    secure: true, // set to true if your using https
     sameSite: "strict", // prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
