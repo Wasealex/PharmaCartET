f98ec6abf72368f4c742dc39128913da735051b3
Wasealex <wasealex27@gmail.com>
2024-09-17 17:47:04 +0300

    refactor: Update server.js to use Helmet for security and set CORS origin in production
    - Import the Helmet package for security measures
    - Use Helmet middleware in the server setup
    - Set the CORS origin to "https://pharmacartet-frontend.onrender.com" in production
    
    refactor: Update package.json to include Helmet as a dependency
    
    - Add "helmet": "^7.1.0" to the dependencies in package.json

--- backend/server.js
+++ backend/server.js
@@ -2,31 +2,45 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import dotenv from "dotenv";
-dotenv.config();
 import cookieParser from "cookie-parser";
+import helmet from "helmet"; // Import Helmet
 import { notFound, errorHandler } from "./middleware/errorMiddleWare.js";
 import connectDB from "./config/db.js";
+
+dotenv.config();
 const port = process.env.PORT || 5000;
+
 import userRoutes from "./routes/userRoutes.js";
 import medicationRoutes from "./routes/medicationRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import paymentRoutes from "./routes/paymentRoutes.js";
 
+// Connect to the database
 connectDB();
+
 const app = express();
 
+// Middleware setup
+app.use(helmet()); // Use Helmet for security
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://pharmacartet-frontend.onrender.com", // Allow requests from this origin
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+);
 
+// Define routes
 app.use("/api/users", userRoutes);
 app.use("/api/medications", medicationRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/payments", paymentRoutes);
 
+// Serve static files in production
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
@@ -40,6 +54,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
+
+// Start the server
 app.listen(port, () => console.log(`Server Started on port ${port}`));
